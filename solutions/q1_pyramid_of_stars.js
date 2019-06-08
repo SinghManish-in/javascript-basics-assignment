bc337596aa6dac6a9008906e157b40367d81a0de
Manish Singh <singhm@Manishs-MacBook-Pro.local>
2019-06-08 22:46:26 +0530

    Q1 Done

--- solutions/q1_pyramid_of_stars.js
+++ solutions/q1_pyramid_of_stars.js
@@ -1,8 +1,30 @@
 /* Write a program to build a `Pyramid of stars` of given height */
 
-const buildPyramid = () => {
-	// Write your code here
+const buildPyramid = (pyramidSize) => {
+    // Write your code here
+    var pyramid = "";
+    for(var i=1;i<pyramidSize+1;i++)
+    pyramid = pyramid + getLeadingSpaces(pyramidSize-i)+getStars(i)+"\n";
+    console.log(result)
 };
+
+const getLeadingSpaces = (pyramidSize) => {
+    var leadingSpaces = "";
+    for(let i=0;i<pyramidSize;i++)
+        leadingSpaces = leadingSpaces + " ";
+    return leadingSpaces;
+};
+
+const getStars = (pyramidSize) => {
+    var stars = "";
+    for(let i=0;i<pyramidSize;i++)
+    stars = stars + "* ";
+    return stars;
+};
+
+
+
+
 
 /* For example,
 INPUT - buildPyramid(6)
@@ -15,5 +37,5 @@ OUTPUT -
 * * * * * *
 
 */
-
+buildPyramid(41);
 module.exports = buildPyramid;
