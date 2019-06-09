919ddb1f333274d7588265b8d9c7c57a010b819f
Manish Singh <manish18.singh18@cognizant.com>
2019-06-09 16:02:31 +0530

    all 5 done

--- solutions/q2_array_to_object_converter.js
+++ solutions/q2_array_to_object_converter.js
@@ -1,13 +1,32 @@
 /* Write a Program to convert an array of objects to an object
 	based on a given key */
-
+	
+const myMap = new Map();
+class MyData {
+	constructor(key, name) {
+		this.key = key;	
+	  	this.name = name;
+	}
+  }
 
 const convert = (json,key) => {
 	// Write your code here
-	var myArray = JSON.parse(json);
-	//console.log(myArray[0]);
-	console.log(key);
+	var prop = key;
+	for(var i=0;i<json.length;i++)
+	{ 	
+		myMap.set(String(json[i].id),json[i]);
+	}
+	console.log(mapToObj(myMap));	
 };
+
+const mapToObj = (inputMap) => {
+    let obj = {};
+    inputMap.forEach(function(value, key){
+        obj[key] = value
+    });
+
+    return obj;
+}
 
 /* For example,
 INPUT - convert([{id: 1, value: 'abc'}, {id: 2, value: 'xyz'}], 'id')
