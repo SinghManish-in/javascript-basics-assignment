44a037088c4ee8726b6a26f12cce01a257adca3c
Manish Singh <manish18.singh18@cognizant.com>
2019-06-09 13:03:45 +0530

    q4 done

--- solutions/q4_data_modelling_and_retrieval.js
+++ solutions/q4_data_modelling_and_retrieval.js
@@ -5,3 +5,17 @@
 
 // Write your code here
 
+const fruits = [{name: 'orange', color: 'orange', pricePerKg: "120"},
+{name: 'mango', color: 'green', pricePerKg: "250"},
+{name: 'apple', color: 'red', pricePerKg: "200"}]
+
+const buildMap = () => {
+    var fruitsMap = new Map();
+    for(var i=0;i<fruits.length;i++)
+        fruitsMap.set(fruits[i].name,fruits[i]);
+    console.log(fruitsMap.get('orange'));
+    console.log(fruitsMap.get('mango'));
+    console.log(fruitsMap.get('apple'));
+}
+
+buildMap();
